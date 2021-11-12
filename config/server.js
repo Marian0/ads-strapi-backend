7ca7781a36292048a608d7452f349fbba866c5de
marian0 <marianosantafe@gmail.com>
2021-11-12 11:33:03 -0300

    Strapi init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2f186d5cf851fab4e6feef24d6395a6a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64f5c75f-08ee-4b38-b7b8-4929769f1fa6'
+};
